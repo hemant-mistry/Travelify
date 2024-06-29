9215def13668dd4caf186dffb0721808de958a64
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-06-30 03:42:00 +0530

    Initializing with tailwindcss

---
+++ frugaloo/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div class="p-4 m-auto border-2 rounded-lg bg-slate-800 text-white">
+      <h1 class="text-3xl font-bold">Hello world!</h1>
+      <p class="mt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam qui
+        minima autem nobis animi perspiciatis deleniti quaerat, recusandae
+        officiis ea totam quisquam repellat quidem perferendis ad! Vel officia
+        voluptatibus ipsam.
+      </p>
+      <button class="bg-indigo-600 hover:bg-indigo-800 mt-4 py-2 px-4 rounded">
+        Click Me
+      </button>
+
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+export default App;
