cd7b911129aa1484dbd17dcc431f9ca3934b3e06
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-06-30 07:56:00 +0530

    Get started and help prompt

--- frugaloo/src/components/PromptInput.jsx
+++ frugaloo/src/components/PromptInput.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import sendIcon from "../assets/sendicon.png"; // Adjust the path based on your folder structure
 import attachmentIcon from "../assets/attachmenticon.png";
+
 function PromptInput() {
   const textareaRef = useRef(null);
 
@@ -15,21 +16,25 @@ function PromptInput() {
   }, []);
 
   return (
-    <div className="fixed bottom-0 w-full p-4 flex items-end">
-      <div className="relative flex"></div>
-      <button className="btn btn-square mr-2">
-      <img src={attachmentIcon} alt="Icon" className="h-6 w-6" />
-      </button>
-      <textarea
-        ref={textareaRef}
-        placeholder="Enter your prompt here..."
-        rows="1"
-        className="textarea textarea-bordered w-full max-h-44 max-w-100 resize-none overflow-auto"
-        onInput={adjustHeight}
-      ></textarea>
-      <button className="btn btn-square ml-2">
-      <img src={sendIcon} alt="Icon" className="h-6 w-6" />
-      </button>
+    <div className="fixed bottom-0 w-full p-4 flex flex-col items-center">
+      <div className="relative flex w-full justify-center mb-4 flex items-end"> {/* Adjusted to flex-col for column layout */}
+        <button className="btn btn-square mr-2">
+          <img src={attachmentIcon} alt="Attachment Icon" className="h-6 w-6" />
+        </button>
+        <textarea
+          ref={textareaRef}
+          placeholder="Enter your prompt here..."
+          rows="1"
+          className="textarea textarea-bordered w-3/4 max-h-44 resize-none overflow-auto"
+          onInput={adjustHeight}
+        ></textarea>
+        <button className="btn btn-square ml-2">
+          <img src={sendIcon} alt="Send Icon" className="h-6 w-6" />
+        </button>
+      </div>
+      <div className="prompt-caution text-center text-xs">
+        Gemini may display inaccurate info, including about people, so double-check its responses. <a className="link link-primary" href="https://support.google.com/gemini?p=privacy_notice">Your privacy and Gemini Apps</a>
+      </div>
     </div>
   );
 }
