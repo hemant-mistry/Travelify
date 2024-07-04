66631ef0f6d8d4a438b0e2c39b49f88b0dec6d97
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-07-04 21:21:41 +0530

    Plan and Plan Input UI minor changes

--- frugaloo/src/components/Plan.jsx
+++ frugaloo/src/components/Plan.jsx
@@ -5,7 +5,7 @@ function Plan() {
   return (
     <>
       <div className="text-center font-bold text-2xl lg:text-3xl">
-        Your personalized Itinerary
+        Your personalized <span className="text-primary">Itinerary..</span>
       </div>
       <div className="timeline-container p-10">
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical bg-green">
@@ -25,7 +25,7 @@ function Plan() {
               </svg>
             </div>
             <div className="timeline-start mb-10 md:text-end">
-              <time className="font-mono italic">1984</time>
+              <time className="font-bold italic text-primary">Day 1</time>
               <div className="text-lg font-black">First Macintosh computer</div>
               The Apple Macintosh—later rebranded as the Macintosh 128K—is the
               original Apple Macintosh personal computer. It played a pivotal
@@ -110,7 +110,7 @@ function Plan() {
               </svg>
             </div>
             <div className="timeline-end mb-10">
-              <time className="font-mono italic">1998</time>
+              <time className="font-bold italic text-primary">Day 2</time>
               <div className="text-lg font-black">iMac</div>
               iMac is a family of all-in-one Mac desktop computers designed and
               built by Apple Inc. It has been the primary part of Apples
@@ -136,7 +136,7 @@ function Plan() {
               </svg>
             </div>
             <div className="timeline-start mb-10 md:text-end">
-              <time className="font-mono italic">2001</time>
+              <time className="font-bold italic text-primary">Day 3</time>
               <div className="text-lg font-black">iPod</div>
               The iPod is a discontinued series of portable media players and
               multi-purpose mobile devices designed and marketed by Apple Inc.
@@ -165,7 +165,7 @@ function Plan() {
               </svg>
             </div>
             <div className="timeline-end mb-10">
-              <time className="font-mono italic">2007</time>
+              <time className="font-bold italic text-primary">Day 4</time>
               <div className="text-lg font-black">iPhone</div>
               iPhone is a line of smartphones produced by Apple Inc. that use
               Apples own iOS mobile operating system. The first-generation
@@ -194,7 +194,7 @@ function Plan() {
               </svg>
             </div>
             <div className="timeline-start mb-10 md:text-end">
-              <time className="font-mono italic">2015</time>
+              <time className="font-bold italic text-primary">Day 5</time>
               <div className="text-lg font-black">Apple Watch</div>
               The Apple Watch is a line of smartwatches produced by Apple Inc.
               It incorporates fitness tracking, health-oriented capabilities,
@@ -218,14 +218,18 @@ function Plan() {
                 />
               </svg>
             </div>
-            <div className="timeline-start mb-10 md:text-end">
-              <time className="font-mono italic">2015</time>
-              <div className="text-lg font-black">Apple Watch</div>
-              The Apple Watch is a line of smartwatches produced by Apple Inc.
-              It incorporates fitness tracking, health-oriented capabilities,
-              and wireless telecommunication, and integrates with iOS and other
-              Apple products and services
+            <div className="timeline-end mb-10">
+              <time className="font-bold italic text-primary">Day 6</time>
+              <div className="text-lg font-black">iPhone</div>
+              iPhone is a line of smartphones produced by Apple Inc. that use
+              Apples own iOS mobile operating system. The first-generation
+              iPhone was announced by then-Apple CEO Steve Jobs on January 9,
+              2007. Since then, Apple has annually released new iPhone models
+              and iOS updates. As of November 1, 2018, more than 2.2 billion
+              iPhones had been sold. As of 2022, the iPhone accounts for 15.6%
+              of global smartphone market share
             </div>
+            <hr />
           </li>
         </ul>
       </div>
--- frugaloo/src/components/PlanInput.jsx
+++ frugaloo/src/components/PlanInput.jsx
@@ -13,9 +13,9 @@ function PlanInput() {
   };
   return (
     <>
-      <div className="mt-5">
+      <div className="mt">
         <div className="text-2xl md:text-3xl font-bold pl-4 md:pl-6">
-          Tell us more about your trip..
+          Tell us more about your <span className="text-primary">trip..</span>
         </div>
         <div className="pl-4 pr-6 pt-2 md:pl-6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt amet
@@ -26,7 +26,7 @@ function PlanInput() {
         <div>
           <label className="form-control w-full max-w-xs">
             <div className="label">
-              <span className="label-text text-sm md:text-lg text-white">
+              <span className="label-text text-sm md:text-sm text-white">
                 Enter your stay details :
               </span>
             </div>
@@ -41,37 +41,96 @@ function PlanInput() {
         <div>
           <label className="form-control w-full max-w-sm md:max-w-lg">
             <div className="label">
-              <span className="label-text text-sm md:text-lg text-white">
+              <span className="label-text text-sm md:text-sm text-white">
                 How many days are you planning to stay:
               </span>
             </div>
-            <Datepicker value={value} onChange={handleValueChange} />
+            <Datepicker value={value} onChange={handleValueChange} inputClassName={"input input-bordered w-full h-8"} />
           </label>
         </div>
         <br />
+
         <div>
-          <div className="label">
-            <span className="label-text text-sm md:text-lg text-white">
-              Enter your budget (in USD):
-            </span>
-          </div>
-          <label className="input input-bordered flex items-center gap-2 max-w-sm md:max-w-lg">
-            <input type="text" className="grow" placeholder="Email" />
-          </label>
-        </div>
-        <br />
-        <div>
-          <label className="form-control w-full max-w-md">
+          <label className="form-control w-full max-w-xs">
             <div className="label">
-              <span className="label-text text-sm md:text-lg text-white">
+              <span className="label-text text-sm md:text-sm text-white">
+                Enter your estimated budget :
+              </span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs input-sm md:input-sm"
+            />
+          </label>
+        </div>
+        <br />
+        <div>
+         
+            <div className="label">
+              <span className="label-text text-sm md:text-sm text-white">
                 Let us know if you got any additional preferences :
               </span>
             </div>
-            <textarea
-              className="textarea textarea-bordered"
-              placeholder="Dump it out"
-            ></textarea>
-          </label>
+            <div className="flex flex-wrap md:flex-nowrap gap-10 mt-5">
+              <div className="flex whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Landmarks</div>
+                
+              </div>
+              <div className="flex whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Rooftop Bars</div>
+                
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Musuems</div>
+            
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Restaurants</div>
+             
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Patio Bars</div>
+        
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Cocktail Bars</div>
+        
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Dance Clubs</div>
+        
+              </div>
+              <div className="flex  whitespace-nowrap">
+              <div>
+                  <input type="checkbox"  className="checkbox" />
+                </div>
+                <div className="ml-2">Desert Shops</div>
+        
+              </div>
+            </div>
+
         </div>
         <div className="divider"></div>
 
